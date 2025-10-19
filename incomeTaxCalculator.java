6fa4e138bc7b8fdaa2a936afc3eb9aa23b7401eb
SaddiHarivardhanReddy <166787099+SaddiHarivardhanReddy@users.noreply.github.com>
2025-10-19 20:24:58 +0530

    incomeTaxCalculator.java

--- incomeTaxCalculator.java
+++ incomeTaxCalculator.java
@@ -9,17 +9,17 @@ public class incomeTaxCalculator {
 		System.out.println("Enter Salary");
 		long salary=s.nextLong();
 		double incomeTax=(salary<=300000)?salary*0.05:(salary>300000 && salary<=1000000)?salary*0.1:salary*0.15;
-		if(salary>2000000) {
-			double bonus=incomeTax*0.01;
-			incomeTax=incomeTax+bonus;
-			System.out.println("Bonus is: "+bonus);
-		}
+		double bonus = (salary > 2000000) ? incomeTax * 0.01 : 0;
+		incomeTax += bonus;
+		if (bonus > 0)
+   		 System.out.println("Bonus is: " + bonus);
 		salary-=incomeTax;
 		System.out.println("Income tax is: "+incomeTax);
-		System.out.println("Salary after tax deduction,after adding bonus: "+salary);
+		System.out.println("Salary after tax and bonus deduction: "+salary);
 		s.close();
 		
 	}
 
 }
+
 
